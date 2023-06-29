3bd5f0f3781e1ea3fd080c302e3f00fced21e833
detker <127975811+detker@users.noreply.github.com>
2023-06-29 23:59:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def load_model_f():
 def preprocess_img(uploaded_img):
     pic = np.array(io.imread(uploaded_img))
     pic_resized = cv2.resize(pic, (150, 150))
-    if len(pic_resized) <= 150*150:
+    if len(pic_resized) == 150*150:
         help = np.zeros((150, 150, 3))
         help[:, :, 0] = pic_resized
         help[:, :, 1] = pic_resized
